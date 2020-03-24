af4105a386a90550881398098492139a55163d06
rpidugu <rpidugu99@gmail.com>
2020-03-24 21:17:25 +0100

    eks-workers.tf is modified to change autoscaling group configs to inline cluster-autoscaler configurations and also instancetype is changed from "t2.large" to "t2.medium"

--- tf-eks-demo/eks-workers.tf
+++ tf-eks-demo/eks-workers.tf
@@ -26,7 +26,7 @@ resource "aws_launch_configuration" "demo" {
   associate_public_ip_address = true
   iam_instance_profile = aws_iam_instance_profile.demo-node.name
   image_id = data.aws_ami.eks-worker.id
-  instance_type = "t2.small"
+  instance_type = "t2.medium"
   name_prefix = "terraform-eks-demo"
   security_groups = [aws_security_group.demo-node.id]
   user_data_base64 = base64encode(local.demo-node-userdata)
@@ -37,7 +37,7 @@ resource "aws_launch_configuration" "demo" {
 }
 
 resource "aws_autoscaling_group" "demo" {
-  desired_capacity = 4
+  desired_capacity = 2
   launch_configuration = aws_launch_configuration.demo.id
   max_size = 5
   min_size = 2
